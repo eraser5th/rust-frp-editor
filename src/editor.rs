390bf29b0016809ce79790d1ec4f52df168863b6
eraser5th <83107074+eraser5th@users.noreply.github.com>
2023-10-23 22:59:50 +0900

    step

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,7 @@ use termion::event::Key;
 
 use crate::Document;
 use crate::Position;
+use crate::Row;
 use crate::Terminal;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -63,11 +64,20 @@ impl Editor {
         println!("{}\r", welcome_message);
     }
 
+    pub fn draw_row(&self, row: &Row) {
+        let start = 0;
+        let end = self.terminal.size().width as usize;
+        let row = row.render(start, end);
+        println!("{}\r", row)
+    }
+
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
+            if if let Some(row) = self.document.row(terminal_row as usize) {
+              self.draw_row(row);
+            } else if row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
