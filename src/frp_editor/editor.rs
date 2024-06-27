115cc21cdb41b01ad3272bc77e53f62fa0f357ff
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-06-27 14:37:15 +0900

    some change

--- src/frp_editor/editor.rs
+++ src/frp_editor/editor.rs
@@ -1,4 +1,5 @@
 use core::panic;
+use std::sync::Arc;
 
 use sodium_rust::Cell;
 use sodium_rust::CellLoop;
@@ -13,8 +14,8 @@ use super::Terminal;
 pub struct Editor {
     cursor_position: Cell<Position>,
     should_quit: bool,
-    keyboard: Keyboard,
-    terminal: Terminal,
+    keyboard: Arc<Keyboard>,
+    terminal: Arc<Terminal>,
 }
 
 impl Editor {
@@ -22,33 +23,44 @@ impl Editor {
         Self {
             cursor_position: Cell::new(&sodium_ctx, Position::default()),
             should_quit: false,
-            keyboard: Keyboard::new(&sodium_ctx),
-            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            keyboard: Arc::new(Keyboard::new(&sodium_ctx)),
+            terminal: Arc::new(Terminal::default().expect("Failed to initialize terminal")),
         }
     }
 }
 
 impl Editor {
-    pub fn run(&mut self, sodium_ctx: &SodiumCtx) {
+    pub fn run<'a>(&'a self, sodium_ctx: &'a SodiumCtx) {
         Terminal::clear_screen();
+        Terminal::cursor_position(&Position::default());
         Terminal::flush().unwrap();
 
         sodium_ctx.transaction(|| {
-            let value: CellLoop<Position> = sodium_ctx.new_cell_loop();
-            Operational::updates(&value.cell()).listen(|p: &Position| Terminal::cursor_position(p));
+            let cursor_position: CellLoop<Position> = sodium_ctx.new_cell_loop();
+            Operational::updates(&cursor_position.cell())
+                .listen(|p: &Position| Terminal::cursor_position(p));
 
-            let update = self.keyboard.key_pressed.snapshot(
-                &value.cell(),
-                |k: &Key, v: &Position| match k {
-                    Key::Char('q') => panic!("q pressed"),
+            let update = self
+                .keyboard
+                .key_pressed
+                .snapshot(&cursor_position.cell(), |k: &Key, v: &Position| match k {
                     Key::Down => v.down(),
                     Key::Up => v.up(),
                     Key::Left => v.left(),
                     Key::Right => v.right(),
                     _ => v.clone(),
-                },
-            );
-            value.loop_(&update.hold(Position::default()));
+                })
+                .filter(|p: &Position| {
+                    p.x < self.terminal.size().width as usize
+                        && p.y < self.terminal.size().height as usize
+                });
+
+            cursor_position.loop_(&update.hold(Position::default()));
+        });
+
+        self.keyboard.key_pressed.listen(|k: &Key| match k {
+            Key::Char('q') => panic!("q pressed"),
+            _ => (),
         });
 
         loop {
