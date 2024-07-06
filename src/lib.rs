2d34accd411ceb8b8319e2ba7462fcd07746396e
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-07 03:03:19 +0900

    FRPではないeditorを削除

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,2 @@
-pub mod document;
-pub mod editor;
 pub mod frp_editor;
-pub mod position;
-pub mod prompt;
-pub mod row;
-pub mod status_line;
-pub mod status_message;
-pub mod terminal;
 pub mod utils;
-
-pub use document::Document;
-pub use position::Position;
-pub use row::Row;
-pub use status_line::StatusLine;
-pub use status_message::StatusMessage;
-pub use terminal::Terminal;
